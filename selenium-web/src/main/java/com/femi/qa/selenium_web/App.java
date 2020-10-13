d863f1207782c576c093ed0571e94251163595af
hp <hp@DESKTOP-V9I2HOB>
2020-10-13 13:42:15 +0100

    Selenium-Web in GitHub

---
+++ selenium-web/src/main/java/com/femi/qa/selenium_web/App.java
@@ -0,0 +1,13 @@
+package com.femi.qa.selenium_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
